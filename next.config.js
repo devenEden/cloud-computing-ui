25a7559e75e73967b132ea5e0088e190e471fe20
fetaDeven <devenfeta19@gmail.com>
2023-05-16 13:15:57 +0300

    updated env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: "http://127.0.0.1:9090/api/v1",
+    NEXT_PUBLIC_API_URL: "https://blog-backend-lvne.onrender.com/api/v1",
   },
 };
 
